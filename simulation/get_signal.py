99d4bede19088d825340d4e4354bab0e6121e9a9
Andrem19 <7255591@gmail.com>
2025-08-14 02:05:17 +0100

    d

--- simulation/get_signal.py
+++ simulation/get_signal.py
@@ -6,18 +6,18 @@ import talib
 
 def signal(data, i: int) -> dict:
 
-    # decision = False
-    # dt = datetime.fromtimestamp(data[i][0]/1000)
+    decision = False
+    dt = datetime.fromtimestamp(data[i][0]/1000)
     
-    # hour = dt.hour
-    # minute = dt.minute
-    # close_6h_ago = data[i-420][4]
-    # close_now = data[i-1][4]
-    # if hour == 8 and minute == 59:
+    hour = dt.hour
+    minute = dt.minute
+    close_6h_ago = data[i-420][4]
+    close_now = data[i-1][4]
+    if hour == 8 and minute == 59:
 
-    #     if close_6h_ago*1.005 < close_now:
-    #         decision=True
+        if close_6h_ago*1.005 < close_now:
+            decision=True
     
     return {
-        "decision": True,
+        "decision": decision,
     }
