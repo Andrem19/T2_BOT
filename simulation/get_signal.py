3c45f25633fc0df2fc257b9810a16e41c9e45207
Andrem19 <7255591@gmail.com>
2025-08-19 17:40:04 +0100

    f

--- simulation/get_signal.py
+++ simulation/get_signal.py
@@ -6,18 +6,14 @@ import talib
 
 def signal(data, i: int) -> dict:
 
-    # decision = False
-    # dt = datetime.fromtimestamp(data[i][0]/1000)
+    decision = False
+    dt = datetime.fromtimestamp(data[i][0]/1000)
     
-    # hour = dt.hour
-    # minute = dt.minute
-    # close_6h_ago = data[i-420][4]
-    # close_now = data[i-1][4]
-    # if hour == 8 and minute == 59:
-
-    #     if close_6h_ago*1.005 < close_now:
-    #         decision=True
+    hour = dt.hour
+    weekday = dt.weekday()
+    if hour in [11, 12] and weekday in [0,1,2,3,4]:
+        decision = True
     
     return {
-        "decision": True,
+        "decision": decision,
     }
